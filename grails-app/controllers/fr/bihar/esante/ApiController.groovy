d01cee915a32fe547240f230ff768ca6818f81cf
Gregory Galli <greg.galli@gmail.com>
2023-12-19 13:35:54 +0100

    Corrections on ApiController

--- grails-app/controllers/fr/bihar/esante/ApiController.groovy
+++ grails-app/controllers/fr/bihar/esante/ApiController.groovy
@@ -9,38 +9,32 @@ class ApiController {
 
     /**
      * Répondra à l'appel sur /api/user/{id}
-     * Devra gérer les requête de type GET, PUT, PATCH, DELETE
-     */
-    def user()
-    {
+     * Devra gérer les requête de type GET, PUT, PATCH, DELETE*/
+    def user() {
         // Avant de commencer on récupère l'utilisateur sur lequel on va travailler
         // et on vérifie que l'utilisateur ne soit pas null
-        if (!params.id)
-            return response.status = 400
+        if (!params.id) return response.status = 400
         User userInstance = User.get(params.id)
-        if (!userInstance)
-            return response.status = 404
+        if (!userInstance) return response.status = 404
+        def paramz = null
 
-        switch (request.getMethod())
-        {
+        switch (request.getMethod()) {
             case "GET":
                 renderThis(userInstance, request.getHeader("Accept"))
                 break
             case "PUT":
-            def paramz
-            // request.getJSON() contiendra les paramètres contenu dans le body si la requête est formatée en JSON
-            if (request.getHeader('Content-Type')) {
-                if (request.getHeader('Content-Type').contains("json"))
-                    paramz = request.getJSON()
-                else if (request.getHeader('Content-Type').contains("xml"))
-                    paramz = request.getXML()
-            }
+                // request.getJSON() contiendra les paramètres contenu dans le body si la requête est formatée en JSON
+                if (request.getHeader('Content-Type')) {
+                    if (request.getHeader('Content-Type').contains("json")) paramz = request.getJSON()
+                    else if (request.getHeader('Content-Type').contains("xml")) paramz = request.getXML()
+                }
 
-            if (!paramz)
-                render(status: 400, text: [status: 400, message: "Missing parameters in body"] as JSON)
-                return
+                if (!paramz) {
+                    render(status: 400, text: [status: 400, message: "Missing parameters in body"] as JSON)
+                    return
+                }
 
-            render "ok"
+                render "ok"
                 break
             case "PATCH":
                 break
@@ -54,37 +48,29 @@ class ApiController {
 
     /**
      * Répondra à l'appel sur /api/users
-     * Devra gérer les requête de type GET, POST
-     */
-    def users()
-    {
+     * Devra gérer les requête de type GET, POST*/
+    def users() {
 
     }
 
 
     /**
      * Répondra à l'appel sur /api/consultation/{id}
-     * Devra gérer les requête de type GET, PUT, PATCH, DELETE
-     */
-    def consultation()
-    {
+     * Devra gérer les requête de type GET, PUT, PATCH, DELETE*/
+    def consultation() {
 
     }
 
 
     /**
      * Répondra à l'appel sur /api/consultations
-     * Devra gérer les requête de type GET, POST
-     */
-    def consultations()
-    {
+     * Devra gérer les requête de type GET, POST*/
+    def consultations() {
 
     }
 
-    def renderThis(Object instance, String accept)
-    {
-        switch (accept)
-        {
+    def renderThis(Object instance, String accept) {
+        switch (accept) {
             case "xml":
             case "application/xml":
             case "text/xml":
